93a120a93a18662b4ed35db66e3cf7d6b4157b55
hysk13 <152942677+hysk13@users.noreply.github.com>
2024-02-25 21:46:07 -0800

    v0.1 release
    - Added main webpage portion
    - Now music can be played! (Thanks, YouTube!)
    - Added JavaScript to dynamically render the pages/sections of the website.
    - Fixed some bugs

---
+++ js/data.js
@@ -0,0 +1,96 @@
+const musicData = [
+    {
+        "title": "First Kiss and My Heart is 120BPM",
+        "by": "Kyoung Seo",
+        "file": "ijfnkGDLM1g",
+        "by-color": "purple",
+        id: 0,
+    }, 
+    {
+        "title": "Ongoing",
+        "by": "Kyoung Seo",
+        "file": "GArleOgvtjs",
+        "by-color": "purple",
+        id: 1,
+    }, 
+    {
+        "title": "Dear My X",
+        "by": "Kyoung Seo",
+        "file": "S1JB_T6Fths",
+        "by-color": "purple",
+        id: 2,
+    },
+    {
+        "title": "Love Actually",
+        "by": "Daybreak + Sunnyhill",
+        "file": "MStMdxnrYyY",
+        "by-color": "orange",
+        id: 3,
+    },
+    {
+        "title": "Discord",
+        "by": "QWER",
+        "file": "WGm2HmXeeRI",
+        "by-color": "white",
+        id: 4,
+    }, 
+    {
+        "title": "Secret Diary",
+        "by": "QWER",
+        "file": "MPK7oP1--4s",
+        "by-color": "white",
+        id: 5,
+    }, 
+    {
+        "title": "Harmony of Stars",
+        "by": "QWER",
+        "file": "On6Pm4M-dQQ",
+        "by-color": "white",
+        id: 6,
+    },
+    {
+        "title": "Your Shampoo Scent in the Flowers",
+        "by": "Jang Bum Joon",
+        "file": "ToUcHgWT8i0",
+        "by-color": "brown",
+        id: 7,
+    },
+    {
+        "title": "HoRangPungRyuGa",
+        "by": "SangRokSu (feat Narae)",
+        "file": "fBQhY7tfx4k",
+        "by-color": "pink",
+        id: 8,
+    },
+    {
+        "title": "Geu ShiJeol",
+        "by": "Hitchhiking Dolphins",
+        "file": "8C5N4geJGX0",
+        "by-color": "blue",
+        id: 9,
+    },
+];
+
+const devMusic = [
+    {
+        "title": "First Kiss and My Heart is 120BPM",
+        "by": "Kyoung Seo",
+        "file": "ijfnkGDLM1g",
+        "by-color": "purple",
+        id: 0,
+    }, 
+    {
+        "title": "HoRangPungRyuGa",
+        "by": "SangRokSu (feat Narae)",
+        "file": "fBQhY7tfx4k",
+        "by-color": "pink",
+        id: 8,
+    },
+    {
+        "title": "Geu ShiJeol",
+        "by": "Hitchhiking Dolphins",
+        "file": "8C5N4geJGX0",
+        "by-color": "blue",
+        id: 9,
+    },
+];
